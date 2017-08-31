cf00cabbf4d0b4657231ec072fd30ded8a820f30
Ivan Notaros <nothke@gmail.com>
2017-09-01 00:10:48 +0200

    Cleanup

--- GLThingy/Mesh.cpp
+++ GLThingy/Mesh.cpp
@@ -25,49 +25,6 @@ Mesh::Mesh(Vertex* vertices, unsigned int numVertices, unsigned int* indices, un
 	}
 
 	InitMesh(model);
-
-	/*
-	m_drawCount = numIndices;
-
-	glGenVertexArrays(1, &m_vertexArrayObject);
-	glBindVertexArray(m_vertexArrayObject);
-
-	vector<vec3> positions;
-	vector<vec2> uvs;
-
-	positions.reserve(numVertices);
-	uvs.reserve(numVertices);
-
-	for (unsigned int i = 0; i < numVertices; i++)
-	{
-		positions.push_back(*vertices[i].GetPos());
-		uvs.push_back(*vertices[i].GetUV());
-	}
-
-	glGenBuffers(NUM_BUFFERS, m_vertexArrayBuffers);
-
-	// POS
-	glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[POSITION_VB]);
-	glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(positions[0]), &positions[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	// UV
-	//unsigned int uvSize = numVertices * sizeof(uvs[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[TEXCOORD_VB]);
-	glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(uvs[0]), &uvs[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-	// INDEX
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_vertexArrayBuffers[INDEX_VB]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, numIndices * sizeof(indices[0]), &indices[0], GL_STATIC_DRAW);
-
-
-	glBindVertexArray(0);
-	*/
 }
 
 Mesh::Mesh(const std::string & fileName)
